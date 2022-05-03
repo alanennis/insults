b37bfdb31770ba220722be8a7171244970349daa
Alan Ennis <alanennis@linuxmail.com>
2022-05-03 20:50:27 +0100

    1.0

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
+use rand::Rng;
 use std::fs::File;
 use std::io::Read;
-use rand::Rng;
-use rand::rngs::ThreadRng;
 
 fn lines_from_file(filename: &str) -> Vec<String> {
     let mut file = match File::open(filename) {
         Ok(file) => file,
-        Err(_) => panic!("no such file"),
+        Err(_) => panic!("no such file {}", filename),
     };
     let mut file_contents = String::new();
     file.read_to_string(&mut file_contents)
         .ok()
         .expect("failed to read!");
-    let lines: Vec<String> = file_contents.split("\n")
+    let lines: Vec<String> = file_contents
+        .split("\n")
         .map(|s: &str| s.to_string())
         .collect();
     lines
@@ -20,21 +20,23 @@ fn lines_from_file(filename: &str) -> Vec<String> {
 
 fn give_rand(line_range: usize) -> usize {
     rand::thread_rng().gen_range(0..line_range)
-
 }
 
 fn main() {
     let file_name_1 = "./shake1.txt";
     let file_name_2 = "./shake2.txt";
     let file_name_3 = "./shake3.txt";
+
     let shake1 = lines_from_file(file_name_1);
     let shake2 = lines_from_file(file_name_2);
     let shake3 = lines_from_file(file_name_3);
-    println!("{:?}", shake1.len());
-    // println!("{}", file[9]);
-    // let mut rng = rand::thread_rng();
-    // println!("Integer: {}", rng.gen_range(0..file.len()));
-    let line_rand = give_rand(shake1.len());
-    println!("{}", shake1[line_rand]);
 
+    let line_rand_1 = give_rand(shake1.len() - 1);
+    let line_rand_2 = give_rand(shake2.len() - 1);
+    let line_rand_3 = give_rand(shake2.len() - 1);
+
+    println!(
+        "{} {} {}",
+        shake1[line_rand_1], shake2[line_rand_2], shake3[line_rand_3]);
+    
 }
